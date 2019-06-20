34695fc2ef40c1d898b7a647d29696abef977dac
dfitzgerald7 <dfitzge1@conncoll.edu>
2019-06-20 18:25:27 -0400

    added getLast method

--- linkedList.java
+++ linkedList.java
@@ -4,15 +4,35 @@ public class LinkedList {
 
     public LinkedList(Node node) {
         head = node;
-    } 
-
-    void push(Node node){
+    }
+    
+    Node getNode(){
         Node current = head;
         while (current.next != null){
             current = current.next;
         }
+        return current;
+    }
+
+    void push(Node node){
+        Node current = this.getNode();
         current.next = node;
     }
+
+    
+    Node pop() {
+        Node current = this.getLast();
+
+    }
+
+    void unshift(Node node){
+        node.next = head;
+        head = node; 
+    }
+
+    // Node shift() {
+    //     return ;
+    // }
 
     void print() {
         Node current = head;
@@ -40,8 +60,14 @@ class LinkedListTest {
         second.next = third;
         myList.print();
 
+        System.out.println("Push");
         Node fourth = new Node(4);
         myList.push(fourth);
+        myList.print();
+
+        System.out.println("Unshift");
+        Node fifth = new Node(5);
+        myList.unshift(fifth);
         myList.print();
     }
 }
