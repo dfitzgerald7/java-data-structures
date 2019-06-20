90ed02ffb5a94096ee8bb8cdf88ed374df2b1166
dfitzgerald7 <dfitzge1@conncoll.edu>
2019-06-20 18:13:48 -0400

    added test class and push

--- linkedList.java
+++ linkedList.java
@@ -1,9 +1,48 @@
 
 public class LinkedList {
-    Node root; 
+    Node head; 
 
-    public static void main(String[] args){
+    public LinkedList(Node node) {
+        head = node;
+    } 
+
+    void push(Node node){
+        Node current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+        current.next = node;
+    }
+
+    void print() {
+        Node current = head;
+        System.out.println("Printing List: ");
+        while(current != null){
+            System.out.println(current.id);
+            current = current.next; 
+        }
         
     }
-    
+
+    public static void main(String[] args){
+        System.out.println("Test 2");
+    }
 }
+
+
+class LinkedListTest {
+    public static void main(String[] args){
+        Node head = new Node(1);
+        LinkedList myList = new LinkedList(head);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        head.next = second;
+        second.next = third;
+        myList.print();
+
+        Node fourth = new Node(4);
+        myList.push(fourth);
+        myList.print();
+    }
+}
+
--- node.java
+++ node.java
@@ -6,6 +6,7 @@ public class Node {
 
     Node(int num) {
         id = num;
+        next = null;
     }
 }
 
